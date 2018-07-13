ae9bf44fd4789a3bda93bb8ecc9bad76907ad393
ChalapathiRevanth <princechalapathi16@gmail.com>
2018-07-13 17:26:19 +0530

    Rosy: Set vendor security patch level & Update Blobs List Matches Xiaomi stock V9.5.11.0.NDAMIFA

--- device.mk
+++ device.mk
@@ -308,7 +308,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.qcom.post_boot.sh
-
+	
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2018-04-01
+	
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
