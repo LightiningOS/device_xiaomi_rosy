e4eb95a86ab676c26ce28c4f551d1742118b3568
ChalapathiRevanth <princechalapathi16@gmail.com>
2018-07-18 18:46:57 +0530

    Rosy: Build Kernel From Source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,10 +47,12 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/zImage-dtb
+TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
+TARGET_KERNEL_CONFIG := rosy_defconfig
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8953
--- device.mk
+++ device.mk
@@ -22,7 +22,3 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-
-# Recovery
-PRODUCT_COPY_FILES += \
-    device/xiaomi/rosy/zImage-dtb:kernel
